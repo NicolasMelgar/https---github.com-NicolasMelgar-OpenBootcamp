bd4e818062f4be5ec5aef647e2310c70b051b489
Nicolas Melgar <elnico521@hotmail.com>
2023-05-11 06:09:18 -0300

    quinto

---
+++ ejercicio5.js
@@ -0,0 +1,58 @@
+let a = 5.4;
+let b = 3.2;
+let suma = a + b;
+let resta = a - b;
+let mult = a * b;
+let div = a / b;
+
+
+console.log(suma)
+console.log(resta)
+console.log(mult)
+console.log(div)
+
+//convertir un numero a string
+
+let a_string = a.toString();
+console.log(a_string);
+console.log(typeof a);
+console.log(typeof a_string);
+
+let c = 235.123456789;
+let d = 150052153;
+//toFixed limita los decimales
+console.log(c);
+console.log(c.toFixed(4));
+console.log(typeof c);
+console.log(typeof c.toFixed(4)); //cambia de numero a string
+
+//toPrecision - me devuelve las cifras significativas
+
+console.log(c.toPrecision(5));
+console.log(typeof c.toPrecision(5));
+// incluso me lo devuelve en notación científica
+console.log(d.toPrecision(6))
+
+
+//--------metodos y límites ----//
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ ejercicio5b.js
@@ -0,0 +1,24 @@
+let alturaCM = 178;
+console.log(alturaCM);
+
+let alturaM = 1.78;
+console.log(alturaM);
+
+let peso = 56.8;
+console.log(peso);
+
+let alturaRed = alturaM.toFixed(1);
+console.log(alturaRed);
+
+//let pesoRed = Math.round(peso);
+let pesoRed = parseInt(peso);
+console.log(pesoRed);
+
+let texto = "el máximo valor que se puede obtener en Javascript es: ";
+let Max_valor_JS = Number.MAX_VALUE;
+let Min_valor_JS = Number.MIN_VALUE;
+let min_precision_JS = Number.EPSILON;
+
+console.log(texto+Max_valor_JS)
+
+
