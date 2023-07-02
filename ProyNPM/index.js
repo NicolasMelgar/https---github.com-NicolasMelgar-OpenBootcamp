96d267c08f55588dc31ef9c617cc9ef525842d35
Nicolas Melgar <elnico521@hotmail.com>
2023-07-02 08:19:27 -0300

    ejercicio 9 en carpeta proy npm

--- ProyNPM/index.js
+++ ProyNPM/index.js
@@ -1,29 +1,48 @@
 const winston = require('winston');
 
 const logger = winston.createLogger({
-  level: 'info',
-  format: winston.format.json(),
-  defaultMeta: { service: 'user-service' },
-  transports: [
-    //
-    // - Write all logs with importance level of `error` or less to `error.log`
-    // - Write all logs with importance level of `info` or less to `combined.log`
-    //
-    new winston.transports.File({ filename: 'error.log', level: 'error' }),
-    new winston.transports.File({ filename: 'combined.log' }),
-  ],
+    level: 'debug',
+    format: winston.format.json(),
+    defaultMeta: { service: 'user-service' },
+    transports: [
+        //
+        // - Write all logs with importance level of `error` or less to `error.log`
+        // - Write all logs with importance level of `info` or less to `combined.log`
+        //
+        new winston.transports.File({ filename: 'error.log', level: 'error' }),
+        new winston.transports.File({ filename: 'combined.log' }),
+    ],
 });
 
 
 
-console.log("hola estoy en pantalla ")
+//logger.log("hola estoy en pantalla ")
 
-console.info("Esto es un mensaje informatico")
+logger.info("Esto es un mensaje informatico")
 
-console.debug("Esto es un mensaje de debug")
+logger.debug("Esto es un mensaje de debug")
 
-console.warn("Esto es una advertencia")
+logger.warn("Esto es una advertencia")
 
-console.error("Esto es un error")
+logger.error("Esto es un error")
 
+/////////////--------------------//////////////////////
 
+const miFuncion3 = val => {
+
+    if(typeof val === "number"){
+        return 2 * val
+    }
+    try{
+    // parte del código que puede fallar 
+        miFuncion3(7)
+        console.log("Está ejecutándose de forma correcta")
+    
+    } catch (e){
+    //En caso de falla quiero que ejecute 
+    console.error("ERROR!")
+    console.error(`El valor de e es: ${e}`)
+    }
+    }
+    
+    console.log(miFuncion3("Hola"))
