bde77ce632dfafe752942e267cb360481ed8fdaa
Nicolas Melgar <elnico521@hotmail.com>
2023-07-02 08:06:19 -0300

    instale winston desde npm

---
+++ ProyNPM/index.js
@@ -0,0 +1,29 @@
+const winston = require('winston');
+
+const logger = winston.createLogger({
+  level: 'info',
+  format: winston.format.json(),
+  defaultMeta: { service: 'user-service' },
+  transports: [
+    //
+    // - Write all logs with importance level of `error` or less to `error.log`
+    // - Write all logs with importance level of `info` or less to `combined.log`
+    //
+    new winston.transports.File({ filename: 'error.log', level: 'error' }),
+    new winston.transports.File({ filename: 'combined.log' }),
+  ],
+});
+
+
+
+console.log("hola estoy en pantalla ")
+
+console.info("Esto es un mensaje informatico")
+
+console.debug("Esto es un mensaje de debug")
+
+console.warn("Esto es una advertencia")
+
+console.error("Esto es un error")
+
+
