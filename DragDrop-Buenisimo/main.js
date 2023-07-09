a967088f430d3cc9e0a64f932f351cc7b4b222e3
Nicolas Melgar <elnico521@hotmail.com>
2023-07-09 11:53:21 -0300

    Dragging

--- DragDrop-Buenisimo/main.js
+++ DragDrop-Buenisimo/main.js
@@ -1,1 +1,36 @@
-alert("Este será el ejercicio de Drag && Drop. Usar porque está buenísimo")
+const parrafos = document.querySelectorAll(".parrafo");
+const secciones = document.querySelectorAll(".seccion")
+
+parrafos.forEach(parrafo => {
+    parrafo.addEventListener("dragstart", event =>{
+        console.log("Inicio de arrastre")
+        console.log("Estoy arrastrando el párrafo: " + parrafo.innerText)
+        parrafo.classList.add("dragging")
+        event.dataTransfer.setData("id", parrafo.id)
+        const elemento_fantasma = document.querySelector(".imagen-fantasma")
+        event.dataTransfer.setDragImage(elemento_fantasma, 0, 0)
+    })
+    parrafo.addEventListener("dragend", ()=> {
+        //console.log("He terminado de arrastrar")
+        parrafo.classList.remove("dragging")
+    })
+})
+
+secciones.forEach(seccion => {
+        seccion.addEventListener("dragover", event =>{
+            event.preventDefault()
+            // puedo usar copy move link y cambia el iconito con el símbolo chico
+            event.dataTransfer.dropEffect = "copy"
+           // console.log("Drag Over")
+            //
+        })
+    seccion.addEventListener("drop", event => {
+        console.log("Drop")
+        const id_parrafo = event.dataTransfer.getData("id")
+        console.log("Parrafo id: ", id_parrafo)
+        //con esto lo puedo mover a otro lado de la sección como un hijo
+        const parrafo = document.getElementById(id_parrafo)
+        seccion.appendChild(parrafo)
+    })
+})
+
