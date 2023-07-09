a211983d96b680b1f89ea84328ec6d42c9e61a72
Nicolas Melgar <elnico521@hotmail.com>
2023-07-09 08:49:35 -0300

    Persistencia Datos HTML

--- Persistencia/index.js
+++ Persistencia/index.js
@@ -1,1 +1,32 @@
-alert("Esto lo tengo que volver a hacer mirando el video")
+//LOCAL STORAGE
+
+localStorage.setItem("nombre", "Nicolás")
+
+localStorage.setItem("apellido", "García")
+
+console.log(localStorage.getItem("nombre"));
+console.log(localStorage.getItem("apellido"));
+
+localStorage.setItem("persona", JSON.stringify({nombre: "Nicolas", apellido: "García"}))
+
+console.log(JSON.parse(localStorage.getItem("persona")))
+
+//JSON.stringify => convierte un objeto /array en string
+
+//JSON.parse convierto un objeto/array en un objeto de JS 
+
+localStorage.removeItem("nombre")
+
+
+//SESION STORAGE
+
+sessionStorage.setItem("nombre-sesion", "Nicolas")
+console.log("nombre-sesion")
+
+//COKIES
+
+document.cookie = "nombreCookie=NicolasCookie";
+document.cookie = "nombreCaducidad = Nombre; expires=" + new Date(2024, 0, 1).toUTCString()
+
+console.log(document.cookie);
+
